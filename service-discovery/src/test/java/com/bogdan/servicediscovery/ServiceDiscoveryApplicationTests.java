8a1e7de165ed3a10413d70171b6953ca10b99786
Bogdan <vasielscubogdan1@gmail.com>
2024-08-07 18:12:44 +0300

    service-discovery: added

---
+++ service-discovery/src/test/java/com/bogdan/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bogdan.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // main
+    }
+
+}
